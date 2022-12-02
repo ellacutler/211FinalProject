923568ee696a62de1dd8f17f953ade7487272e46
ellacutler <98005116+ellacutler@users.noreply.github.com>
2022-12-02 11:10:07 -0600

    Adding the rest of the tests
    All 5 tests are here, will update more later today to add more to them! 
    - line 125 is both depending on what randomly comes up

--- test/model_test.cxx
+++ test/model_test.cxx
@@ -10,6 +10,8 @@ using namespace ge211;
 using Position = ge211::Posn<int>;
 using Dimensions = ge211::Dims<int>;
 using Position_set = std::vector<Position>;
+
+/// TEST ACCESS STRUCTS AND MEMBER FUNCTIONS
 struct Test_access
 {
     Model& model;
@@ -20,9 +22,16 @@ struct Test_access
     void set_all_positions(int i);
     void print_board() const;
     Position_set all_positions() const;
+    void set_manual_positions(const std::vector<int> &vect);
+    bool game_over();
+    void next_turn();
 
 };
 //TEST ACCESS MEMBER FUNCTIONS!
+void
+Test_access::next_turn(){
+    model.next_turn_();
+}
 Model::Position_set
 Test_access::all_positions() const
 {
@@ -47,15 +56,23 @@ Test_access::print_board() const
 {
     model.print_board();
 }
+void
+Test_access::set_manual_positions(const std::vector<int> &vect)
+{
+    model.set_manual_positions(vect);
+
+}
+bool
+Test_access::game_over(){
+    return model.gameover_;
+}
 
 TEST_CASE("example test (TODO: replace this)")
 {
     CHECK(1 + 1 == 2);
 }
 
-//
-// TODO: Write preliminary model tests.
-// TODO: Model tests for
+///MODEL TESTS
 
 TEST_CASE(" aa starting condition test")
 {
@@ -103,11 +120,75 @@ TEST_CASE("bb complex merging test")
     CHECK(model.get_score() == 64);
     model.play_move({0,1});
     model.print_score();
-    CHECK(model.get_score() == 96);
+    // this one can either be 100 or 96 depending only on what is randomly
+
+    CHECK((model.get_score() == 96 || model.get_score() == 100));
     model.print_board();
-    // this section checks that illegal moves don't do any
+}
+TEST_CASE( "game win ")
+{
+    Model model(4,4, {1,1}, {2,2} );
+    Test_access access(model);
+    access.set_manual_positions(
+            {1024,512, 0, 0,
+             0,256, 0, 0,
+             0,256, 0, 0,
+             0,0, 0, 0}
+            );
 
-
+    access.print_board();
+    model.play_move({0,-1});
+    model.play_move({0,-1});
+    CHECK(model.get_won()== false);
+    model.play_move({-1,0});
+    CHECK(model.get_won() == true);
+    CHECK(model.is_gameover()==true);
 
 }
+TEST_CASE("game lose")
+{
+    Model model(4,4, {1,1}, {2,2} );
+    Test_access access(model);
+    access.set_manual_positions(
+            {256,16,32, 4,
+             4,128, 1024, 8,
+             32,0, 8, 64,
+             256,16, 256, 8}
+    );
+
+    access.print_board();
+    model.play_move({0,1});
+    access.print_board();
+
+
+    CHECK(model.get_won()== false);
+    CHECK(access.is_board_mergable()==false);
+    CHECK(access.game_over() == true);
+
+}
+TEST_CASE("game merge 2")
+{
+    Model model(4,4, {1,1}, {2,2} );
+    Test_access access(model);
+    access.set_manual_positions(
+            {256,128,32, 4,
+             4,128, 4, 8,
+             4,0, 8, 64,
+             256,16, 256, 8}
+    );
+
+    access.print_board();
+    model.play_move({0,-1});
+    model.play_move({-1,0});
+
+    access.print_board();
+
+
+    CHECK(model.get_won()== false);
+    CHECK(access.is_board_mergable()==true);
+    CHECK(access.game_over() == false);
+
+}
+
+
 
